14c45defad7f4cebf0009dd806432cea97803c3c
roccogiordano <rocco.giordano573@gmail.com>
2024-10-13 22:57:38 +0200

    feat: added comments to the code

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// Variables
+
 const kmPrice = 0.21;
 
 let price;
+
+//
+
+
+// Distance Input
 
 let distance = Number(prompt("Inserisci il numero di KM da percorrere"));
 
@@ -8,11 +15,21 @@ while (isNaN(distance) || distance === 0) {
     distance = Number(prompt("Inserisci una distanza valida!"))
 }
 
+//
+
+
+// Age Input
+
 let age = Number(prompt("Inserisci l'età del viaggiatore"));
 
 while (isNaN(age) || age === 0) {
     age = Number(prompt("Inserisci un'età valida!"))
 }
+
+//
+
+
+// Price Calculator
 
 if (age < 18) {
     price = kmPrice * distance / 100 * 80
@@ -24,6 +41,13 @@ else {
     price = kmPrice * distance
 }
 
+//
+
+
+// Price Output
+
 price = Math.round(price * 100) / 100
 
 window.alert(`Il prezzo del tuo biglietto è di ${price}\u20AC`)
+
+//
